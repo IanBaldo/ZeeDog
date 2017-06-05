15a59f0ce978390281c35f2f403278bc481b217f
Ian Baldo <ian.baldo@am4.com.br>
2017-06-04 21:26:45 -0300

    form alert + minor changes

--- task2/js/app.js
+++ task2/js/app.js
@@ -1,6 +1,6 @@
 angular.module('zeedog', [])
 
-.controller('mainCtrl', function ($scope) {
+.controller('mainCtrl', function ($scope, $timeout) {
     var emailList;
     $scope.availableImages = [
         'img/king_jambo_frente.jpg',
@@ -11,7 +11,11 @@ angular.module('zeedog', [])
 
     $scope.mainImg = $scope.availableImages[0];
     $scope.userEmail = '';
-    $scope.emailValidated = true;  // Necessário para a bprda não começar vermelha
+    $scope.alert ={
+        show: false,
+        type: '',
+        message: ''
+    };
     
     // Se já existir uma lista de e-mails cadastrados, carrega a lista
     if (localStorage.getItem('emailList') != null) {
@@ -21,13 +25,17 @@ angular.module('zeedog', [])
         emailList = [];
     }
 
+    // Verifica e Registra o e-mail
     $scope.registerUser = function () {
         if(/\S+@\S+\.\S+/.test($scope.userEmail)){
             // E-mail parece válido
             // Podemos adicionar na lista
-
-            $scope.emailValidated = true;
             emailList.push($scope.userEmail);
+
+            // Prepara e exibe o alerta por 5 segundos
+            $scope.alert.message = 'E-mail cadastrado com sucesso!';
+            $scope.alert.type = 'alert-success';
+            showAlert(5);
 
             // Pode limpar o campo
             $scope.userEmail = '';
@@ -37,8 +45,10 @@ angular.module('zeedog', [])
         }
         else {
             // E-mail falhou na validação
-            alert('falhoiu');
-            $scope.emailValidated = false;
+            // Prepara e exibe o alerta por 5 segundos
+            $scope.alert.message = 'E-mail inválido';
+            $scope.alert.type = 'alert-danger';
+            showAlert(5);
         }
 
         console.log('Usuários Registrados:\n');
@@ -50,6 +60,16 @@ angular.module('zeedog', [])
 
     $scope.changeImage = function (imgPath) {
         $scope.mainImg = imgPath;
+    }
+
+    // Exibe o alerta por 'timeout' segundos
+    function showAlert(timeout) {
+        console.log($scope.alert);
+        $scope.alert.show = true;
+        console.log($scope.alert);
+        $timeout(function() {
+            $scope.alert.show = false
+        }, 1000 * timeout);
     }
 
 })
